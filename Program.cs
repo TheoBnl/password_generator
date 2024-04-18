8e01cd35e41976fe2098facf704998b138d5ae58
TheoBnl <99369307+TheoBnl@users.noreply.github.com>
2024-04-18 23:33:29 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace password_generator
 {
     internal static class Program
     {
-        /// <summary>
-        /// Point d'entrée principal de l'application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
